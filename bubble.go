c693db78f7c85de946682d95636f3f564b78849e
Jexulie <fejitj3n@yahoo.com>
2018-12-11 18:01:36 +0300

    .

--- bubble.go
+++ bubble.go
@@ -1,24 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	_ "image/jpeg"
+	"image/png"
 	"os"
 )
 
+// TO8BIT x
 const TO8BIT = 257
 
+// ImgProps x
 type ImgProps struct {
 	width       int
 	height      int
 	colorMatrix []Point
 }
 
+// Point p
 type Point struct {
 	x     int
 	y     int
 	color RGBCOLOR
+}
+
+func (i ImgProps) colorAt(x, y int) RGBCOLOR {
+	for _, v := range i.colorMatrix {
+		if v.x == x && v.y == y {
+			return v.color
+		}
+	}
+	return RGBCOLOR{0, 0, 0}
 }
 
 func readImage(path string) image.Image {
@@ -34,7 +46,8 @@ func readImage(path string) image.Image {
 	return imgData
 }
 
-func setProps(img image.Image) ImgProps {
+// SetProps p
+func SetProps(img image.Image) ImgProps {
 	var iProps ImgProps
 	r := img.Bounds()
 	iProps.width = r.Size().X
@@ -52,8 +65,25 @@ func setProps(img image.Image) ImgProps {
 
 func main() {
 	img := readImage("kawaii.jpg")
-	p := setProps(img)
-	for _, k := range p.colorMatrix {
-		fmt.Println(ConvertToHSL(k.color))
-	}
+	p := SetProps(img)
+	// for _, k := range p.colorMatrix {
+	// 	fmt.Println(ConvertToHSL(k.color))
+	// }
+
+	l := ScaleImage(10, p)
+	f, _ := os.Create("2xScaled.png")
+	png.Encode(f, l)
+
+	// im := image.NewRGBA(image.Rect(0, 0, p.width, p.height))
+	// for x := 0; x < p.width; x++ {
+	// 	for y := 0; y < p.height; y++ {
+	// 		c := p.colorAt(x, y)
+	// 		c1 := color.RGBA{uint8(c.R), uint8(c.G), uint8(c.B), 0xff}
+	// 		im.Set(x, y, c1)
+	// 	}
+	// }
+
+	// f, _ := os.Create("clone.png")
+	// png.Encode(f, im)
+
 }
--- manupulate.go
+++ manupulate.go
@@ -1,6 +1,105 @@
 package main
 
-// ConvertToGrayscale
-func ConvertToGrayscale() {
+import (
+	"image"
+	"image/color"
+	"log"
+	"os"
+)
 
+func isEven(num int) bool {
+	if num%2 == 0 {
+		return true
+	}
+	return false
+}
+
+// ConvertToGrayscale a
+// func ConvertToGrayscale(img image.Image) image.Image {
+
+// }
+
+// ScaleImage s
+func ScaleImage(times int, img ImgProps) image.Image {
+	// if its square
+	// what if not ? :(
+	isEven := isEven(times)
+	pixelsEach := (times - 1)
+	if times < 0 {
+		log.Fatalln("Times must be positive")
+		os.Exit(1)
+	}
+	im := image.NewRGBA(image.Rect(0, 0, img.width*times, img.height*times))
+	for x := 0; x < img.width*times; x++ {
+		for y := 0; y < img.height*times; y++ {
+			if isEven {
+				if x%2 == 0 && y%2 == 0 {
+					if x == 0 || y == 0 {
+						c := img.colorAt(0, 0)
+						c1 := color.RGBA{uint8(c.R), uint8(c.G), uint8(c.B), 0xff}
+						im.Set(x, y, c1)
+					} else {
+						c := img.colorAt(x/times, y/times)
+						c1 := color.RGBA{uint8(c.R), uint8(c.G), uint8(c.B), 0xff}
+						im.Set(x, y, c1)
+					}
+				} else {
+					c := img.colorAt(x/times, y/times)
+					c1 := color.RGBA{uint8(c.R), uint8(c.G), uint8(c.B), 0xff}
+					im.Set(x, y, c1)
+
+					for h := 1; h < pixelsEach; h++ {
+						im.Set(x+h, y, c1)
+						im.Set(x, y+h, c1)
+						im.Set(x+h, y+h, c1)
+					}
+				}
+			} else {
+				if x%2 != 0 && y%2 != 0 {
+					if x == 0 || y == 0 {
+						c := img.colorAt(0, 0)
+						c1 := color.RGBA{uint8(c.R), uint8(c.G), uint8(c.B), 0xff}
+						im.Set(x, y, c1)
+					} else {
+						c := img.colorAt(x/times, y/times)
+						c1 := color.RGBA{uint8(c.R), uint8(c.G), uint8(c.B), 0xff}
+						im.Set(x, y, c1)
+					}
+				} else {
+					c := img.colorAt(x/times, y/times)
+					c1 := color.RGBA{uint8(c.R), uint8(c.G), uint8(c.B), 0xff}
+					im.Set(x, y, c1)
+
+					for h := 1; h < pixelsEach; h++ {
+						im.Set(x+h, y, c1)
+						im.Set(x, y+h, c1)
+						im.Set(x+h, y+h, c1)
+					}
+				}
+			}
+		}
+	}
+	return im
+}
+
+// CropImage x
+func CropImage(Ox, Oy, Px, Py int, img ImgProps) *image.RGBA {
+	if Ox+Px > img.width {
+		log.Fatalln("Wrong Parameters")
+		os.Exit(1)
+	}
+
+	if Oy+Py > img.height {
+		log.Fatalln("Wrong Parameters")
+		os.Exit(1)
+	}
+	im := image.NewRGBA(image.Rect(0, 0, Px, Py))
+	for x := 0; x < Px; x++ {
+		for y := 0; y < Py; y++ {
+			c := img.colorAt(x+Ox, y+Oy)
+			c1 := color.RGBA{uint8(c.R), uint8(c.G), uint8(c.B), 0xff}
+			im.Set(x, y, c1)
+		}
+	}
+	return im
 }
